f073fa153595a01fa00a85c5a9dc7b2ab7bae21d
Jordan Ward <jordancward@gmail.com>
2024-09-25 09:04:37 -0600

    Create PDF for each Excel file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import openpyxl
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # openpyxl is a Python library to read/write Excel files
 # glob allows you to search for files with specific patterns
@@ -9,6 +11,20 @@ import glob
 invoices = glob.glob("invoices/*.xlsx")
 
 for excel_file in invoices:
-    # this pandas command only works with openpxyl included
+
+    # this pandas command only works with openpxyl downloaded
     df = pd.read_excel(excel_file, sheet_name="Sheet 1")
-    print(df)
+
+    # create pdf for each excel_file
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    # extracting the name of the file
+    filename = Path(excel_file).stem
+    invoice_nr = filename.split("-")[0]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+
+    # placing each file in a folder
+    pdf.output(f"PDFs/{filename}.pdf")
