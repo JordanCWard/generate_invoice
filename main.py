dfb3a939e2e52243eb2c421faf3377cb508a26ee
Jordan Ward <jordancward@gmail.com>
2024-09-27 14:15:11 -0600

    Add invoice date

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import openpyxl
 import glob
 from fpdf import FPDF
 from pathlib import Path
+# import openpyxl
 
 # openpyxl is a Python library to read/write Excel files
 # glob allows you to search for files with specific patterns
@@ -12,7 +12,7 @@ invoices = glob.glob("invoices/*.xlsx")
 
 for excel_file in invoices:
 
-    # this pandas command only works with openpxyl downloaded
+    # this pandas command only works with openpxyl downloaded, see imports
     df = pd.read_excel(excel_file, sheet_name="Sheet 1")
 
     # create pdf for each excel_file
@@ -21,10 +21,16 @@ for excel_file in invoices:
 
     # extracting the name of the file
     filename = Path(excel_file).stem
-    invoice_nr = filename.split("-")[0]
+
+    # extract the invoice number and date from the filename
+    invoice_nr, date = filename.split("-")
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+
+    # ln adds 1 break line after this cell
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
 
     # placing each file in a folder
     pdf.output(f"PDFs/{filename}.pdf")
