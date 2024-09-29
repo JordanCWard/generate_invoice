bae10e1709fa27de0ab4bdeb271dc489a09844bf
Jordan Ward <jordancward@gmail.com>
2024-09-29 10:49:43 -0600

    Add example of multiple lines in a cell

---
+++ multi_cell_example.py
@@ -0,0 +1,17 @@
+from fpdf import FPDF
+
+# add multiple lines in a cell
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.add_page()
+
+content = """
+Lorem ipsum dolor sit amet, consectetur adipiscing 
+elit, sed do eiusmod tempor incididunt ut labore 
+et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco.
+"""
+
+pdf.set_font(family="Times", size=12)
+pdf.multi_cell(w=0, h=6, txt=content, border=1)
+pdf.output("output.pdf")
